3392fae52a22755c2c29a44184bd455adf5b45a7
Sandra Schaftner <83032118+SandraSchaftner@users.noreply.github.com>
2021-06-11 16:35:39 +0200

    Update Question.svelte

--- src/Question.svelte
+++ src/Question.svelte
@@ -1,16 +1,16 @@
 <script>
-   // the keyword "export" is used to pass props to children 
+   	// the keyword "export" is used to pass props to children 
 	export let questionText;
 </script>
 
 <style> 
 	/* h1 styling is scoped to Question component. There is no conflict with h1 in App component. */
    h1 {
-		 font-family: 'Overpass', sans-serif;
-		 text-align: center;
-     color: #000;
-		 padding: 1em 1em;
-		 font-size: 1.2em;
+	font-family: 'Overpass', sans-serif;
+	text-align: center;
+     	color: #000;
+	padding: 1em 1em;
+	font-size: 1.2em;
    }    
 </style>
 
